9b72768b16e9f0655cf997555a5cdb3c990d9183
jasperyzh <jasper.yzh@gmail.com>
2021-12-22 20:07:31 +0800

    cleanup and updated mckl-openday

--- src/js/main.js
+++ src/js/main.js
@@ -8,14 +8,16 @@ import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap-select";
 
 // import js
-import "./components/Testimonial.js";
+// import "./components/Testimonial.js";
 import "./components/ScrollToSection.js";
 
 // vue
 import Vue from 'vue';
-import LeadForm from './components/LeadForm';
-import Webinar from './components/Webinar';
+import LeadForm from './vue/LeadForm';
+import Testimonial from './vue/Testimonial';
+// import Webinar from './components/Webinar';
 
-new Vue({ render: createElement => createElement(LeadForm) }).$mount('#leadform');
-new Vue({ render: createElement => createElement(Webinar) }).$mount('#webinar');
+new Vue({ render: createElement => createElement(Testimonial) }).$mount('#testimonial-vue');
+new Vue({ render: createElement => createElement(LeadForm) }).$mount('#leadform-vue');
+// new Vue({ render: createElement => createElement(Webinar) }).$mount('#webinar');
 
---
+++ src/js/vue/Testimonial.vue
@@ -0,0 +1,270 @@
+<template>
+  <div class="testimonial-vue">
+    <article class="testimonial">
+      <div class="d-flex flex-wrap">
+        <div class="col-md-7 d-flex align-items-center">
+          <blockquote class="blockquote">
+            <p>
+              “ADTP@MCKL has given me a realistic expectation of how education
+              in the United States is. We were taught and graded with an
+              equivalent standard, preparing us for the quality and critical
+              thinking level abroad. MCKL also prepared us in terms of both U.S.
+              academics and lifestyle.”
+            </p>
+            <footer class="testimonial__footer h4">
+              Sarah Marie Alicia Anthony <br /><small
+                >American Degree Transfer Program, MCKL</small
+              >
+              <br /><small
+                >Theology &amp; Criminology, Loyola University Chicago, USA</small
+              >
+            </footer>
+          </blockquote>
+        </div>
+        <div class="col-md-5">
+          <img
+            src="../../img/testimonial-sarah.jpg"
+            alt="Sarah Marie Alicia Anthony"
+          />
+        </div>
+      </div>
+    </article>
+
+    <article class="testimonial">
+      <div class="d-flex flex-wrap">
+        <div class="col-md-7 d-flex align-items-center">
+          <blockquote class="blockquote">
+            <p>
+              “ADTP@MCKL not only emphasises on your academics but the ability
+              to express yourself. I was able to study at Oxford University
+              through my study abroad programme at Messiah College. Overall, it
+              was a humbling experience being able to learn at top universities
+              around the world.”
+            </p>
+            <footer class="testimonial__footer h4">
+              Ethan Wong <br /><small
+                >American Degree Transfer Program, MCKL</small
+              >
+              <br /><small
+                >Computer Science, Messiah College, USA on Provost Scholarship
+              </small>
+            </footer>
+          </blockquote>
+        </div>
+        <div class="col-md-5">
+          <img src="../../img/testimonial-ethanwong.jpg" alt="Ethan Wong" />
+        </div>
+      </div>
+    </article>
+    <article class="testimonial">
+      <div class="d-flex flex-wrap">
+        <div class="col-md-7 d-flex align-items-center">
+          <blockquote class="blockquote">
+            <p>
+              “I first completed my F1 paper at a tuition provider and was then
+              moving around different colleges to complete my ACCA studies
+              according to the lecturers’ capability, but after completing my
+              F6, F7 and F8 in MCKL, I decided to continue my ACCA journey here
+              due to the facilities, environment and especially the many friends
+              I made.”
+            </p>
+            <footer class="testimonial__footer h4">
+              Tee Jia En <br /><small>ACCA, MCKL</small> <br /><small
+                >RCE Capital Berhad</small
+              >
+            </footer>
+          </blockquote>
+        </div>
+        <div class="col-md-5">
+          <img src="../../img/testimonial-jia_ern.jpg" alt="Tee Jia En" />
+        </div>
+      </div>
+    </article>
+    <article class="testimonial">
+      <div class="d-flex flex-wrap">
+        <div class="col-md-7 d-flex align-items-center">
+          <blockquote class="blockquote">
+            <p>
+              “My choice to study in MCKL was truly an excellent decision as I
+              had dedicated lecturers who encouraged me to excel, leading to me
+              being awarded the Cambridge Outstanding Learners Award for the
+              October/ November 2019 examination series as Top in the World for
+              Chemistry AS level. I will recommend MCKL without hesitation to
+              any prospective students.”
+            </p>
+            <footer class="testimonial__footer h4">
+              Jesselyn Sin Jun Yi <br /><small
+                >Bachelor of Medical Science and Doctor of Medicine,</small
+              >
+              <br /><small>Monash University Australia</small>
+            </footer>
+          </blockquote>
+        </div>
+        <div class="col-md-5">
+          <img
+            src="../../img/testimonial-jesselyn.jpg"
+            alt="Jesselyn Sin Jun Yi"
+          />
+        </div>
+      </div>
+    </article>
+    <article class="testimonial">
+      <div class="d-flex flex-wrap">
+        <div class="col-md-7 d-flex align-items-center">
+          <blockquote class="blockquote">
+            <p>
+              “I was convinced to join MCKL for its strong reputation and
+              affordable fees. Starting off with the AUSMAT programme, I begin
+              to see college life is about both learning and developing a robust
+              social life. I successfully achieved ATAR 97.85, while also
+              emerging as frisbee champions at the Malaysia University Ultimate
+              Championships 2019.”
+            </p>
+            <footer class="testimonial__footer h4">
+              Jilian Tai Ju-En <br /><small
+                >Australian Matriculation, MCKL</small
+              >
+              <br /><small>Physiotherapy, Curtin University, Australia</small>
+            </footer>
+          </blockquote>
+        </div>
+        <div class="col-md-5">
+          <img
+            src="../../img/testimonial-jilian.jpg"
+            alt="testimonial-jilian"
+          />
+        </div>
+      </div>
+    </article>
+    <article class="testimonial">
+      <div class="d-flex flex-wrap">
+        <div class="col-md-7 d-flex align-items-center">
+          <blockquote class="blockquote">
+            <p>
+              “I always wanted to help in social work but at times I feel
+              incapable because I do not know what the right approaches are.
+              After joining DSW@MCKL, it helped me to be professional when doing
+              social work, nonjudgmental, more competent and respectful at the
+              same time.”
+            </p>
+            <footer class="testimonial__footer h4">
+              Angelia Gan Lee Fan <br /><small
+                >Diploma in Social Work, MCKL</small
+              >
+            </footer>
+          </blockquote>
+        </div>
+        <div class="col-md-5">
+          <img
+            src="../../img/testimonial-angelia.jpg"
+            alt="testimonial-angelia"
+          />
+        </div>
+      </div>
+    </article>
+    <article class="testimonial">
+      <div class="d-flex flex-wrap">
+        <div class="col-md-7 d-flex align-items-center">
+          <blockquote class="blockquote">
+            <p>
+              “MCKL’s programme has helped me look at social work from a wider
+              perspective. Prior to this, I solely depend on my limited
+              experience and basic internal training skills. The course has
+              given me the ability to improve my practice at every level, right
+              from initial contact, assessment, case planning, residential care,
+              community-based recovery and beyond.”
+            </p>
+            <footer class="testimonial__footer h4">
+              Vijayan Ramasami <br /><small>Diploma in Social Work, MCKL</small>
+            </footer>
+          </blockquote>
+        </div>
+        <div class="col-md-5">
+          <img src="../../img/testimonial-vija.jpg" alt="Vijiyan Ramasami" />
+        </div>
+      </div>
+    </article>
+    <article class="testimonial">
+      <div class="d-flex flex-wrap">
+        <div class="col-md-7 d-flex align-items-center">
+          <blockquote class="blockquote">
+            <p>
+              “Part of the memorable experience at MCKL was being taught by kind
+              and diligent lecturers. They presented lectures in an
+              easy-to-understand way to accommodate students from various
+              backgrounds and proficiency levels of English language while never
+              compromising on the quality of their classes.”
+            </p>
+            <footer class="testimonial__footer h4">
+              Denise Thean Pui Yan <br /><small
+                >Diploma in Early Childhood Education, MCKL</small
+              >
+              <br /><small
+                >Bachelor of Early Childhood Care and Education, Institute of
+                Technology, Tralee, Ireland</small
+              >
+            </footer>
+          </blockquote>
+        </div>
+        <div class="col-md-5">
+          <img
+            src="../../img/testimonial-denise.jpg"
+            alt="testimonial-denise"
+          />
+        </div>
+      </div>
+    </article>
+    <article class="testimonial">
+      <div class="d-flex flex-wrap">
+        <div class="col-md-7 d-flex align-items-center">
+          <blockquote class="blockquote">
+            <p>
+              “Despite starting school only at the age of nine, I now have the
+              highest education qualification in my family from Kampung Perupuk
+              Pitas, Sabah. Thanks to MCKL, I now have a Diploma in Early
+              Childhood Education and am currently able to work in the city to
+              support my family.”
+            </p>
+            <footer class="testimonial__footer h4">
+              Ranimah Oransum <br /><small
+                >Diploma in Early Childhood Education, MCKL</small
+              >
+            </footer>
+          </blockquote>
+        </div>
+        <div class="col-md-5">
+          <img src="../../img/testimonial-ranimah.jpg" alt="Ranimah Oransumg" />
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+import { tns } from "tiny-slider/src/tiny-slider";
+
+export default {
+  mounted() {
+    // testimonials
+    tns({
+      container: ".testimonial-vue",
+      items: 1,
+      slideBy: "page",
+      autoplay: true,
+      loop: true,
+      // fixedWidth: true,
+      // autoWidth: true,
+      viewportMax: true,
+      arrowKeys: true,
+      autoplayButtonOutput: false,
+      controlsPosition: "bottom",
+      navPosition: "bottom",
+      autoHeight: true,
+      controls: false,
+      autoplayTimeout: 8000,
+      autoplayHoverPause: true,
+      mouseDrag: true,
+    });
+  },
+};
+</script>
