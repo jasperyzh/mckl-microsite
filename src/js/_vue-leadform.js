19bb11024dcc3947cf79b7da192c124cf630ac0c
jasperyzh <jasper.yzh@gmail.com>
2022-02-24 16:41:28 +0800

    updated zohoform, missing gsheetformsubmit

--- src/js/_vue-leadform.js
+++ src/js/_vue-leadform.js
@@ -1,4 +1,12 @@
 
 import Vue from 'vue';
-import LeadForm from './vue/LeadForm';
-new Vue({ render: createElement => createElement(LeadForm) }).$mount('#leadform-vue');
+
+// import LeadForm from './vue/LeadForm';
+// if (document.getElementById("leadform-vue")) {
+//     new Vue({ render: createElement => createElement(LeadForm) }).$mount('#leadform-vue');
+// }
+
+import ZohoWebform from './vue/ZohoWebform';
+if (document.getElementById("ZohoWebform")) {
+    new Vue({ render: createElement => createElement(ZohoWebform) }).$mount('#ZohoWebform');
+}
--- src/js/main-openday.js
+++ src/js/main-openday.js
@@ -1,38 +1,38 @@
 // leadform
 window.SHEET_NAME = "openday";
-window.PROGRAMME_INTERESTED = {
-    "Pre-U / Foundation": {
-        CAL: "Cambridge A Level",
-        AUSMAT: "Australian Matriculation",
-    },
-    "Diploma & Degree": {
-        ADTP: "American Degree Transfer Program",
-        DECE: "Diploma in Early Childhood Education",
-        DSW: "Diploma in Social Work - NEW",
-        DCS: "Diploma in Computer Science - NEW",
-        DIT: "Diploma in Information Technology - NEW",
-        DEC: "Diploma in E-Commerce - NEW",
-        DDM: "Diploma in Digital Marketing - NEW",
-        DFT: "Diploma in Financial Technology - NEW",
-    },
-    "Professional Accounting": {
-        ACCAFIA: "ACCA Foundation in Accountancy",
-        ACCA: "Association of Chartered Certified Accountants",
-    },
-    "Professional Development": {
-        CICTL: "Cambridge International Certificate in Teaching &amp; Learning",
-        CIDTL: "Cambridge International Diploma in Teaching &amp; Learning",
-        ICD: "International Certification of Digital Literacy",
-        MCECE: "Micro-Credentials in Early Childhood Education",
-        MCSW: "Micro-Credentials in Social Work",
-        MCCS: "Micro-credentials in Computer Science",
-        MCIT: "Micro-credentials in Information Technology",
-        MCEC: "Micro-credentials in E-Commerce",
-        MCDM: "Micro-credentials in Digital Marketing",
-        EMLPE: "Educational Management and Leadership for Private Education",
-        TLERSCW: "Teaching &amp; Learning and Education Related Short Courses &amp; Workshop",
-    },
-};
+// window.PROGRAMME_INTERESTED = {
+//     "Pre-U / Foundation": {
+//         CAL: "Cambridge A Level",
+//         AUSMAT: "Australian Matriculation",
+//     },
+//     "Diploma & Degree": {
+//         ADTP: "American Degree Transfer Program",
+//         DECE: "Diploma in Early Childhood Education",
+//         DSW: "Diploma in Social Work - NEW",
+//         DCS: "Diploma in Computer Science - NEW",
+//         DIT: "Diploma in Information Technology - NEW",
+//         DEC: "Diploma in E-Commerce - NEW",
+//         DDM: "Diploma in Digital Marketing - NEW",
+//         DFT: "Diploma in Financial Technology - NEW",
+//     },
+//     "Professional Accounting": {
+//         ACCAFIA: "ACCA Foundation in Accountancy",
+//         ACCA: "Association of Chartered Certified Accountants",
+//     },
+//     "Professional Development": {
+//         CICTL: "Cambridge International Certificate in Teaching &amp; Learning",
+//         CIDTL: "Cambridge International Diploma in Teaching &amp; Learning",
+//         ICD: "International Certification of Digital Literacy",
+//         MCECE: "Micro-Credentials in Early Childhood Education",
+//         MCSW: "Micro-Credentials in Social Work",
+//         MCCS: "Micro-credentials in Computer Science",
+//         MCIT: "Micro-credentials in Information Technology",
+//         MCEC: "Micro-credentials in E-Commerce",
+//         MCDM: "Micro-credentials in Digital Marketing",
+//         EMLPE: "Educational Management and Leadership for Private Education",
+//         TLERSCW: "Teaching &amp; Learning and Education Related Short Courses &amp; Workshop",
+//     },
+// };
 
 // import
 import "./_main-default.js";
